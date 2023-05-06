64eb7ac6d3ea9dd97ea88e742bd43345dd63f6c6
Robert <roberto.arevalocabello@gmail.com>
2023-05-06 11:04:06 -0300

    "24-04-23"

--- UtilidadEjExtra2/src/Main/Main.java
+++ UtilidadEjExtra2/src/Main/Main.java
@@ -3,19 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package utilidadejextra2;
+package Main;
 
+import Entidad.Ahorcado;
+import Servicio.Servicio;
 /**
  *
  * @author flavia
  */
-public class UtilidadEjExtra2 {
-
-    /**
-     * @param args the command line arguments
-     */
+public class Main {
     public static void main(String[] args) {
-        // TODO code application logic here
+        Ahorcado ah=new Ahorcado();
+        Servicio serv=new Servicio();
+        serv.crearJuego(ah);
     }
-    
 }
